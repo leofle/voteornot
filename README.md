f92d2aa8d85bb8c9a80e47335438723dd1904db6
lio <leofle@gmail.com>
2014-04-21 13:57:43 +0300

    upload

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-voteornot
-=========
+vote or not
+====
+this platform of a game of cards, where randomly you see 2 politicians side by side, no names, no parties.
+when you click on one, another apears. when you click majority of a party the game is over.
+=======
+vote
 
-card game
+====
---
+++ js/main.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+	var l = 'licud';
+	var y = 'yeshatid';
+	var m = 'meretz';
+	var a = 'avoda';
+
+
+	var not = $('img').not(this).removeClass;
+
+	var num = '95';
+	var likudNum = '95';
+	var avodaNum = '95';
+	var meretzNum = '95';
+	var yeshNum = '95';
+
+
+	$('#close').click(function(){
+		$('.popup, .pop_holder, span#licud_pop, span#meretz_pop, span#yesh_pop, span#avoda_pop').css('display','none');
+		$('.results').css('display','block');
+		});
+
+
+
+$('.show').click(function(e){
+	e.preventDefault();
+
+			random = Math.ceil(Math.random() * 20);
+			var i = random;
+
+			partieNum = Math.ceil(Math.random() * 4);
+			$('#shp').html(partieNum);
+			if (partieNum == 1) {
+				partie = a;
+			}
+			if (partieNum == 2) {
+				partie = m;
+			}
+			if (partieNum == 3) {
+				partie = l;
+			}
+			if (partieNum == 4) {
+				partie = y;
+			}
+
+
+
+			//if ($('.show').find(this).attr() == ($('.show').not(this).attr())) {
+			//	$('.show').not(this).attr('src', 'img/'+partie+'/'+(i++)+(1)+'.jpg')
+			//}
+
+			if ($(this).hasClass('licud')) {
+				if (i >= 21) {
+					i = random;
+				}
+				if (partie == l) {
+					partie = y;
+				}
+
+				$('#licud').width(likudNum++);
+				$('.show').not(this).removeClass('avoda');
+				$('.show').not(this).removeClass('yeshatid');
+				$('.show').not(this).removeClass('meretz');
+				$('.show').not(this).removeClass('licud');
+				$('.show').not(this).addClass(partie);
+				$('.show').not(this).attr('src', 'img/'+partie+'/'+(i++)+'.jpg');
+			}
+			if ($(this).hasClass('meretz')) {
+				if (i >= 21) {
+					i = random;
+				}
+				if (partie == m) {
+					partie = a;
+				}
+				$('#meretz').width(meretzNum++);
+				$('.show').not(this).removeClass('avoda');
+				$('.show').not(this).removeClass('yeshatid');
+				$('.show').not(this).removeClass('meretz');
+				$('.show').not(this).removeClass('licud');
+				$('.show').not(this).addClass(partie);
+				$('.show').not(this).attr('src', 'img/'+partie+'/'+(i++)+'.jpg');
+			}
+		if ($(this).hasClass('avoda')) {
+				if (i >= 21) {
+					i = random;
+				}
+				if (partie == a) {
+					partie = l;
+				}
+				$('#avoda').width(avodaNum++);
+				$('.show').not(this).removeClass('avoda');
+				$('.show').not(this).removeClass('yeshatid');
+				$('.show').not(this).removeClass('meretz');
+				$('.show').not(this).removeClass('licud');
+				$('.show').not(this).addClass(partie);
+				$('.show').not(this).attr('src', 'img/'+partie+'/'+(i++)+'.jpg');
+			}
+			if ($(this).hasClass('yeshatid')) {
+				if (i >= 21) {
+					i = random;
+				}
+				if (partie == y) {
+					partie = m;
+				}
+				$('#yesh').width(yeshNum++);
+				$('.show').not(this).removeClass('avoda');
+				$('.show').not(this).removeClass('yeshatid');
+				$('.show').not(this).removeClass('meretz');
+				$('.show').not(this).removeClass('licud');
+				$('.show').not(this).addClass(partie);
+				$('.show').not(this).attr('src', 'img/'+partie+'/'+(i++)+'.jpg');
+			}
+
+			if ($('span#licud').width() == 100) {
+				$('.popup').css('display','inline-block');
+				$('.pop_holder').css('display','block');
+				$('span#licud_pop').css('display','block');
+				$('#check').css('top','-113px');
+				$('#check').css('left','118px');
+			}
+			if ($('span#meretz').width() == 100) {
+				$('.popup').css('display','inline-block');
+				$('.pop_holder').css('display','block');
+				$('span#meretz_pop').css('display','block');
+				$('#check').css('top','-24px');
+				$('#check').css('left','118px');
+			}
+			if ($('span#yesh').width() == 100) {
+				$('.popup').css('display','inline-block');
+				$('.pop_holder').css('display','block');
+				$('span#yesh_pop').css('display','block');
+				$('#check').css('top','-54px');
+				$('#check').css('left','118px');
+			}
+			if ($('span#avoda').width() == 100) {
+				$('.popup').css('display','inline-block');
+				$('.pop_holder').css('display','block');
+				$('span#avoda_pop').css('display','block');
+				$('#check').css('top','-84px');
+				$('#check').css('left','118px');
+			}
+
+
+			});
+
+
+
+
+
+});
