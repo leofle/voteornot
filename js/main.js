6748f3b13b0ceded059e3b975d873c059d27bba3
lio <leofle@gmail.com>
2014-04-21 14:43:48 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		var numArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 		for (var i = 1; i < numArray.length; i++) {
 			numArray[i] = new Image();
-			numArray[i].src = "img/licud/"+numArray[i]+".jpg";
+			numArray[i].src = "img/licud/"+i+".jpg";
 		}
 	}
 }
