cd819b7552a83c18e71b3bf65ca9c312ae8ce6a0
liof27 <leofle@gmail.com>
2015-03-19 14:50:18 +0200

    new vars

--- js/main.js
+++ js/main.js
@@ -7,8 +7,13 @@ var a = 'avoda';
 var partieArray = [a,m,l,y];
 var partie;
 var not = $('img').not(this).removeClass;
-num = licudNum = avodaNum = meretzNum = yeshNum = partieActiveNum = '95';
-random = Math.ceil(Math.random() * 20);
+var num = '95';
+var licudNum = '95';
+var avodaNum = '95';
+var meretzNum = '95';
+var yeshNum = '95';
+var partieActiveNum = '95';
+var random = Math.ceil(Math.random() * 20);
 var i = random;
 
 function popChanger(param1) {
